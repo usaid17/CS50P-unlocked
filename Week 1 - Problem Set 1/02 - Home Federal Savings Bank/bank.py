290989fb21c81875e01db7eee4f77716f4e761de
Usaid <hiusaidk@gmail.com>
2025-05-08 13:48:02 +0530

    Update and rename bank.py.md to bank.py

--- Week 1 - Problem Set 1/02 - Home Federal Savings Bank/bank.py
+++ Week 1 - Problem Set 1/02 - Home Federal Savings Bank/bank.py
@@ -5,32 +5,21 @@ def main():
     print(value(user_input))
 
   
-  
-
 def value(greeting):
 
     greeting = greeting.lower().strip()
 
-  
-
     if greeting.startswith("hello"):
-
         return 0
 
   
-
     elif greeting.startswith("h"):
-
         return 20
 
-  
 
     else:
-
         return 100
-
-  
-  
+ 
 
 if __name__ == "__main__":
 
