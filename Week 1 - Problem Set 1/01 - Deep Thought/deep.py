ff82f16145325755d8e23b6ca2a9cd58d6e94612
Usaid <hiusaidk@gmail.com>
2025-05-08 13:46:29 +0530

    Update and rename deep.py.md to deep.py

--- Week 1 - Problem Set 1/01 - Deep Thought/deep.py
+++ Week 1 - Problem Set 1/01 - Deep Thought/deep.py
@@ -1,8 +1,6 @@
 
 question = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-  
-
 match question:
 
     case "42" | "forty-two" | "Forty Two" | "FoRty TwO" | " 42 " | "forty two" | " 42" | "42 " | "42, " | " 42," :
