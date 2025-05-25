f446f48fac57417c46288dc73571daf0b39add69
Usaid <hiusaidk@gmail.com>
2025-05-25 22:48:09 +0530

    Update Interpreter.py

--- Week 1 - Problem Set 1/04 - Math Interpreter/Interpreter.py
+++ Week 1 - Problem Set 1/04 - Math Interpreter/Interpreter.py
@@ -1,43 +1,23 @@
 def main():
 
     expression = input ("Expression: ").strip() # Get user input & remove spaces
-
     x, y, z = expression.split (" ") # Split into three parts: x, operator, z
 
-  
-
     x = float(x)
-
     z = float(z)
 
-  
-
     if y == "+" :
-
-        Result = (x + z)
-
-  
+        Result = (x + z)  
 
     elif y == "-" :
-
         Result = (x - z)
 
-  
-
     elif y == "*" :
-
         Result = (x * z)
 
-  
-
     elif y == "/" :
-
         Result = (x / z)
 
-  
-
     print (f"{Result:.1f}")
-
-  
 
 main()
